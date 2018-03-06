eff812af8c6858354b0cb57d4cd5f7a4b5a34caa
Julien Lamy <lamy@unistra.fr>
2018-03-06 11:37:15 +0100

    Fix for commands with unicode characters

--- spire/runner.py
+++ spire/runner.py
@@ -1,13 +1,12 @@
+import os
 import subprocess
+import tempfile
 
 def run_ninja(ninja_file, raw_ninja_arguments):
     """ Run Ninja using the file generated from the pipeline and the 
         command-line arguments.
     """
     
-    process = subprocess.Popen(
-        ["ninja", "-f", "/dev/stdin"]+raw_ninja_arguments, 
-        stdin=subprocess.PIPE)
-    process.communicate(ninja_file)
-    
-    return process.returncode
+    with tempfile.NamedTemporaryFile() as fd:
+        fd.write(ninja_file.encode("utf-8"))
+        return subprocess.call(["ninja", "-f", fd.name]+raw_ninja_arguments)
