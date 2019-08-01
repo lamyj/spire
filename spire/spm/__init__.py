5c6f5475c2cfcfa420627b636b2c1f8842cceec6
Julien Lamy <lamy@unistra.fr>
2019-08-01 16:24:19 +0200

    Remove ModelReview

--- spire/spm/__init__.py
+++ spire/spm/__init__.py
@@ -1,6 +1,5 @@
 from . import contrast_manager
 from . import factorial_design
 from .model_estimation import ModelEstimation
-from .model_review import ModelReview
 from .spm_object import SPMObject
 from .utils import *
