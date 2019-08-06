ee2a84514001b6a147253afb9d9c53fa6b515c24
Julien Lamy <lamy@unistra.fr>
2019-08-06 10:38:14 +0200

    Add spm.ImageCalculator

--- spire/spm/__init__.py
+++ spire/spm/__init__.py
@@ -1,5 +1,6 @@
 from . import contrast_manager
 from . import factorial_design
+from .image_calculator import ImageCalculator
 from .model_estimation import ModelEstimation
 from .spm_object import SPMObject
 from .stats import Stats
